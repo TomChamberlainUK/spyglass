a85a83d3338c096b5a88d258705803a4bcaba85e
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-09-15 16:47:27 +0100

    Added /articles page, nav, and metadata for html titles

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,21 @@
-import { Card } from 'ui';
+import Link from 'next/link';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home | BBC Spyglass'
+}
 
 export default function Page() {
   return (
     <>
-      <Card />
+      <h1>
+        BBC Spyglass
+      </h1>
+      <nav>
+        <Link href={'/articles'}>
+          Articles
+        </Link>
+      </nav>
     </>
   );
 }
