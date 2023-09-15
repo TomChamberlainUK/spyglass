e6c32d384e1ce719e1dfb12b698dbc6de8b48d8a
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-09-15 12:37:14 +0100

    Fixed issue with ide linter and jest-dom types

--- packages/ui/vitest.config.ts
+++ packages/ui/vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'happy-dom',
-    setupFiles: ['./tests/setup.ts']
+    setupFiles: ['./tests/setupTests.ts']
   }
 });
