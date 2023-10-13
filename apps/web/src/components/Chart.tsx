d14b1095351c48c54df56fa423ed3d9db07c1275
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2023-10-13 13:01:34 +0100

    Fixed missing typescript generics

--- apps/web/src/components/Chart.tsx
+++ apps/web/src/components/Chart.tsx
@@ -47,8 +47,17 @@ export default function LinePlot({
   const x = d3.scaleLinear([0, data.length - 1], [marginLeft, width - marginRight]);
   const y = d3.scaleLinear(d3.extent(data), [height - marginBottom, marginTop]);
   const line = d3.line((d, i) => x(i), y);
-  useEffect(() => void d3.select(gx.current).call(d3.axisBottom(x)), [gx, x]);
-  useEffect(() => void d3.select(gy.current).call(d3.axisLeft(y)), [gy, y]);
+
+  useEffect(() => {
+    d3.select<SVGGElement, {}>(gx.current)
+      .call(d3.axisBottom(x));
+  }, [gx, x]);
+
+  useEffect(() => {
+    d3.select<SVGGElement, {}>(gy.current)
+      .call(d3.axisLeft(y));
+  }, [gy, y]);
+
   return (
     <>
       <svg width={width} height={height}>
